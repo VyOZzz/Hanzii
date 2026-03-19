5edde5d770e63659a9bed4a8194343b480459511
VyOZzz <vyp8269@gmail.com>
2026-03-19 10:53:53 +0700

    create springboot project

---
+++ src/main/java/com/vy/hanzi/hanzi_srs_dictionary/HanziSrsDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.vy.hanzi.hanzi_srs_dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanziSrsDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanziSrsDictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vy/hanzi/hanzi_srs_dictionary/HanziSrsDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vy.hanzi.hanzi_srs_dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanziSrsDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
